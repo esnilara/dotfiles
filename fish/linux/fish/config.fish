f5f2d22346f34647465f5e709d191c6bf1de931e
esnilara <esteban@esnilara.io>
2017-04-23 22:38:46 -0500

    Fish shell: Set vim as default editor

--- fish/linux/fish/config.fish
+++ fish/linux/fish/config.fish
@@ -3,3 +3,9 @@ nvm use default --silent
 
 # fix backtick keyboard
 xmodmap -e 'keycode 49 = grave asciitilde'
+
+# Make Vim the default editor
+set --export EDITOR 'vim -f'
+
+# Make Vim usable with git
+set --export GIT_EDITOR 'vim -f'
