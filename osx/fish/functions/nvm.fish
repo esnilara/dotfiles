84d5c86c9bb838406f238dc481e0aaee556c1cb1
esnilara <esteban@esnilara.io>
2017-05-02 18:14:34 -0500

    Fix nvm function to use correct .nvm folder from brew

--- osx/fish/functions/nvm.fish
+++ osx/fish/functions/nvm.fish
@@ -1,3 +1,8 @@
+# For OSX
+# ~> brew install nvm
+# ~> mkdir ~/.nvm
+# ~> ln -s (brew --prefix nvm)/nvm.sh ~/.nvm/nvm.sh
+
 function nvm --description 'Node version manager'
-  bass source (brew --prefix nvm)/nvm.sh ';' nvm $argv
+  bass source ~/.nvm/nvm.sh ';' nvm $argv
 end
