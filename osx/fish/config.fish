190c3302930cc8727358db8aaf1f56b59bd41954
esnilara <esteban@esnilara.io>
2019-02-02 12:58:34 -0600

    Fish: Update osx fish files

--- osx/fish/config.fish
+++ osx/fish/config.fish
@@ -1,2 +1,7 @@
 source ~/.config/fish/.fish_abbreviations
 source ~/.config/fish/.fish_variables
+source ~/.asdf/asdf.fish
+
+# tabtab source for electron-forge package
+# uninstall by removing these lines or running `tabtab uninstall electron-forge`
+[ -f /Users/esnilara/workspace/nimbox/node_modules/tabtab/.completions/electron-forge.fish ]; and . /Users/esnilara/workspace/nimbox/node_modules/tabtab/.completions/electron-forge.fish
--- osx/fish/functions/nvm.fish
+++
@@ -1,8 +0,0 @@
-# For OSX
-# ~> brew install nvm
-# ~> mkdir ~/.nvm
-# ~> ln -s (brew --prefix nvm)/nvm.sh ~/.nvm/nvm.sh
-
-function nvm --description 'Node version manager'
-  bass source ~/.nvm/nvm.sh ';' nvm $argv
-end
