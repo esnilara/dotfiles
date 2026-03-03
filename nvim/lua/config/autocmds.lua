f0f06e96c6e89f98567804a503d5c3a422815ef5
Esteban Lara <elara@fleetio.com>
2026-03-03 16:49:23 -0600

    Add nvim trim whitespace on save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -23,3 +23,13 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = apply,
 })
+
+-- Trim trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local save = vim.fn.winsaveview()
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.winrestview(save)
+  end,
+})
